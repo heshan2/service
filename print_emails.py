b9a8fcddb50072d1baf52cd46465508816eb92ff
Tharushka heshan <87757215+heshan2@users.noreply.github.com>
2021-08-18 16:58:54 +0300

    Delete print_emails.py

--- print_emails.py
+++
@@ -1,9 +0,0 @@
-  x = json.load(open(file, 'rb'))
-        strr += x['client_email'] + ', '
-        
-        if (count + 1)% 10 == 0:
-            strr = strr[:-2]
-            strr += '\n\n'
-            strr += '-------------------------------------\n\n'
-    strr = strr[:-3]
-    print(strr)
