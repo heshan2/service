fb6560a71f81ee4427a4534c02c05a6b4678bc83
Tharushka heshan <87757215+heshan2@users.noreply.github.com>
2021-08-18 20:07:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-echo copy your credential.json file to SA folder.
-echo after copied press Enter
+echo firs
 read varpause
-cd sa
 pip3 install -U -r requirements.txt
 python3 gen_sa_accounts.py --quick-setup -1
 echo SA accounts generated
 echo click Enter to rename json files
 read varpause1
-python3 0_to_99.py
+python3 rename.py
