12091249a6c09e5439d64ef9cf03954e8563b43d
Tharushka heshan <87757215+heshan2@users.noreply.github.com>
2021-08-18 20:09:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
-echo firs
+clear
+echo press enter to continue.
 read varpause
 pip3 install -U -r requirements.txt
 python3 gen_sa_accounts.py --quick-setup -1
-echo SA accounts generated
+echo Service accounts and token.pickle generated
 echo click Enter to rename json files
 read varpause1
+clear
 python3 rename.py
+echo all json s renamed.
