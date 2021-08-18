b5c24735acbf96a6301e5e18d9b1527bb2637329
Tharushka heshan <87757215+heshan2@users.noreply.github.com>
2021-08-18 17:07:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,6 @@ cd sa
 pip3 install -U -r requirements.txt
 python3 gen_sa_accounts.py --quick-setup -1
 echo SA accounts generated
+echo click Enter to rename json files
+read varpause1
+python3 0_to_99.py
