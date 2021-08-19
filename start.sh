f0f8c10ba5a97380384c9dbfb6357c0d3e04aad5
Tharushka heshan <87757215+heshan2@users.noreply.github.com>
2021-08-19 12:08:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 clear
 echo press enter to continue.
 read varpause
+clear
 pip3 install -U -r requirements.txt
-python3 gen_sa_accounts.py --quick-setup -1
-echo Service accounts and token.pickle generated
+python3 gen_sa_accounts.py
+clear
+echo press enter to view your projects list
+read varpause1
+python3 gen_sa_accounts.py --list-projects
+echo Enter you now using project.
+read varpi
+python3 gen_sa_accounts.py --enable-services $varpi
+python3 gen_sa_accounts.py --create-sas $varpi
+python3 gen_sa_accounts.py --download-keys $varpi
 echo click Enter to rename json files
 read varpause1
 clear
